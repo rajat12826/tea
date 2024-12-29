8817aa0f40a8db2119bb7fac8ae12da9596977e2
rajat12826 <pariharrajat078@gmail.com>
2024-12-29 21:24:39 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js}',
+    './pages/**/*.{html,js}',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        product: ["product","sans-serif"], 
+          
+        },
+    },
+  },
+  plugins: [],
+}
